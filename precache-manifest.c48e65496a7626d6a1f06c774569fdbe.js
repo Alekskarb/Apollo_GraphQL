65a1fe314d795af50320b65f9c67a10063ffb773
Alekskarb <korbutowski@gmail.com>
2020-02-24 10:49:26 +0300

    Updates

---
+++ precache-manifest.c48e65496a7626d6a1f06c774569fdbe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4046755bf72400709812dc15c547a820",
+    "url": "/Apollo_GraphQL/index.html"
+  },
+  {
+    "revision": "97185ae0f7a6b794bac7",
+    "url": "/Apollo_GraphQL/static/js/2.36087dac.chunk.js"
+  },
+  {
+    "revision": "8aaa6f2db9016e0ad829",
+    "url": "/Apollo_GraphQL/static/js/main.fdb5e4b0.chunk.js"
+  },
+  {
+    "revision": "27c368c13416a9e173f4",
+    "url": "/Apollo_GraphQL/static/js/runtime~main.a531cbf6.js"
+  },
+  {
+    "revision": "07d5ba4a894c77d6f799ecde7e84850f",
+    "url": "/Apollo_GraphQL/static/media/cart.07d5ba4a.svg"
+  },
+  {
+    "revision": "a8a3ba3a10003218c2a017cf68d20489",
+    "url": "/Apollo_GraphQL/static/media/curve.a8a3ba3a.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/Apollo_GraphQL/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/Apollo_GraphQL/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/Apollo_GraphQL/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/Apollo_GraphQL/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "3192f86cb3a13b08200f5e3d3fef0325",
+    "url": "/Apollo_GraphQL/static/media/home.3192f86c.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/Apollo_GraphQL/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "0c7bf061e88c942e5031e971c6ab447b",
+    "url": "/Apollo_GraphQL/static/media/logo.0c7bf061.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/Apollo_GraphQL/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "ea589b574fff44db577387da01243704",
+    "url": "/Apollo_GraphQL/static/media/profile.ea589b57.svg"
+  },
+  {
+    "revision": "c27cc1f53fed31cbbc1d743f9bbaba1d",
+    "url": "/Apollo_GraphQL/static/media/rocket.c27cc1f5.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/Apollo_GraphQL/static/media/space.7dfc7923.jpg"
+  }
+]);
